1246538e17415cf2a7343af2d55eeb7b33e7983d
rumbehant <rumbehant@38e6eff2-4521-0410-8f8e-9afd2f83d171>
2009-02-06 00:53:39 +0000

    + Adding new build files + Added patches
    git-svn-id: https://winglib.svn.sourceforge.net/svnroot/winglib/trunk@140 38e6eff2-4521-0410-8f8e-9afd2f83d171

---
+++ dep/lib_abel.mk
@@ -0,0 +1,34 @@
+
+default_target: all
+
+#-------------------------------------------------------------------
+# Project
+#-------------------------------------------------------------------
+PRJ_NAME := abel
+PRJ_TYPE := lib
+PRJ_INCS := 
+PRJ_LIBS := 
+PRJ_DEFS :=
+PRJ_LIBROOT := ..
+
+#-------------------------------------------------------------------
+# Configure build
+#-------------------------------------------------------------------
+include $(PRJ_LIBROOT)/config.mk
+
+#-------------------------------------------------------------------
+# File locations
+#-------------------------------------------------------------------
+export LOC_TAG := def
+LOC_CXX_def := c
+LOC_BLD_def := cpp
+LOC_INC_def := $(CFG_LIBROOT)/abel/lib
+LOC_SRC_def := $(CFG_LIBROOT)/abel/lib
+LOC_EXC_def := serial
+include $(PRJ_LIBROOT)/build.mk
+
+#-------------------------------------------------------------------
+# Execute the build
+#-------------------------------------------------------------------
+include $(PRJ_LIBROOT)/go.mk
+
