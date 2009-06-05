76334088f13eccf33a9dea46920ea1ac5856cbfb
rumbehant <rumbehant@38e6eff2-4521-0410-8f8e-9afd2f83d171>
2009-06-05 20:44:16 +0000

    + Still working on webkit
    git-svn-id: https://winglib.svn.sourceforge.net/svnroot/winglib/trunk@275 38e6eff2-4521-0410-8f8e-9afd2f83d171

--- dep/lib_webkit.mk
+++ dep/lib_webkit.mk
@@ -6,17 +6,26 @@ default_target: all
 #-------------------------------------------------------------------
 PRJ_NAME := WebKit
 PRJ_TYPE := lib
-PRJ_INCS := WebKit wxWidgets/include freetype/include \
-			WebKit/WebCore WebKit/WebCore/editing WebKit/WebCore/dom \
-			WebKit/WebCore/platform WebKit/WebCore/platform/text WebKit/WebCore/icu \
-			WebKit/WebCore/platform/graphics WebKit/WebCore/page WebKit/WebCore/rendering \
-			WebKit/WebCore/loader WebKit/WebCore/rendering/style WebKit/WebCore/css \
-			WebKit/WebCore/platform/network WebKit/WebCore/platform/network/soup \
-			WebKit/WebCore/html WebKit/WebCore/platform/graphics/transforms \
-			WebKit/WebCore/platform/animation WebKit/WebCore/platform/graphics/wx \
-			WebKit/JavaScriptCore WebKit/JavaScriptCore/jit WebKit/JavaScriptCore/interpreter \
-			WebKit/JavaScriptCore/runtime
-PRJ_SYSI := /usr/include/cairo
+PRJ_INCS := wxWidgets/include freetype/include \
+			WebKit WebKit/DerivedSources \
+			WebKit/WebCore WebKit/WebCore/accessibility WebKit/WebCore/bindings/js \
+			WebKit/WebCore/bridge WebKit/WebCore/bridge/c WebKit/WebCore/css WebKit/WebCore/dom \
+			WebKit/WebCore/editing WebKit/WebCore/history WebKit/WebCore/html WebKit/WebCore/inspector \
+			WebKit/WebCore/loader WebKit/WebCore/loader/appcache WebKit/WebCore/loader/archive WebKit/WebCore/loader/icon \
+			WebKit/WebCore/page WebKit/WebCore/page/animation WebKit/WebCore/platform WebKit/WebCore/platform/animation \
+			WebKit/WebCore/platform/graphics WebKit/WebCore/platform/graphics/filters WebKit/WebCore/platform/graphics/transforms \
+			WebKit/WebCore/platform/network WebKit/WebCore/platform/text WebKit/WebCore/plugins WebKit/WebCore/rendering \
+			WebKit/WebCore/rendering/style WebKit/WebCore/workers WebKit/WebCore/xml WebKit/WebCore/bindings/js \
+			WebKit/WebCore/platform/sql WebKit/WebCore/storage WebKit/WebCore/svg WebKit/WebCore/svg/animation \
+			WebKit/WebCore/svg/graphics WebKit/WebCore/svg/graphics/filters \
+			\
+			WebKit/JavaScriptCore WebKit/JavaScriptCore/API WebKit/JavaScriptCore/ForwardingHeaders WebKit/JavaScriptCore/interpreter \
+			WebKit/JavaScriptCore/bytecode WebKit/JavaScriptCore/bytecompiler WebKit/JavaScriptCore/debugger WebKit/JavaScriptCore/jit \
+			WebKit/JavaScriptCore/pcre WebKit/JavaScriptCore/profiler WebKit/JavaScriptCore/runtime WebKit/JavaScriptCore/wrec \
+			WebKit/JavaScriptCore/assembler WebKit/JavaScriptCore/wtf/unicode WebKit/JavaScriptCore/yarr WebKit/JavaScriptCore/pcre \
+			WebKit/JavaScriptCore/parser WebKit/JavaScriptCore/runtime WebKit/JavaScriptCore/wtf
+
+PRJ_SYSI := 
 PRJ_LIBS := 
 PRJ_DEFS := USE_FREETYPE
 
@@ -32,13 +41,23 @@ UNSUPPORTED := Set make option BUILD_WEBKIT=1 to build
 include $(PRJ_LIBROOT)/unsupported.mk
 else
 
-ifeq ($(OS),win32)
+ifeq ($(PLATFORM),windows)
 	PRJ_LIBS := $(PRJ_LIBS) wxWidgets
 	PRJ_DEFS := 
+	
+	PRJ_INCS := winglib/dep/etc/WebKit/inc/windows $(PRJ_INCS)
+
 else
 	PRJ_EXTC := `wx-config --cppflags`
 	PRJ_EXTL := `wx-config --libs`
 #	PRJ_OSLB := X11 GLU Xxf86vm
+
+	PRJ_INCS := winglib/dep/etc/WebKit/inc/posix $(PRJ_INCS) \
+				WebKit/WebKit/gtk WebKit/WebKit/gtk/WebCoreSupport WebKit/WebKit/gtk/webkit \
+				WebKit/WebCore/platform/gtk WebKit/WebCore/platform/graphics/gtk \
+				WebKit/WebCore/accessibility/gtk WebKit/WebCore/loader/gtk WebKit/WebCore/page/gtk \
+				WebKit/WebCore/platform/graphics/cairo WebKit/WebCore/platform/network/soup
+
 endif
 
 #-------------------------------------------------------------------
@@ -49,17 +68,15 @@ endif
 # -- JavaScriptCore
 #----------------------------
 
-export LOC_TAG := wc_ac
-#LOC_INC_wc_ac := $(CFG_LIBROOT)/WebKit/WebCore/accessibility
-LOC_SRC_wc_ac := $(CFG_LIBROOT)/WebKit/WebCore/accessibility
+#export LOC_TAG := wc_ac
+#LOC_SRC_wc_ac := $(CFG_LIBROOT)/WebKit/WebCore/accessibility
 #LOC_LST_wc_ac := AllInOneFile
-include $(PRJ_LIBROOT)/build.mk
-
-#export LOC_TAG := jsc_api
-#LOC_INC_jsc_api := $(CFG_LIBROOT)/WebKit/JavaScriptCore
-#LOC_SRC_jsc_api := $(CFG_LIBROOT)/WebKit/JavaScriptCore
-#LOC_LST_jsc_api := AllInOneFile
 #include $(PRJ_LIBROOT)/build.mk
+
+export LOC_TAG := jsc_api
+LOC_SRC_jsc_api := $(CFG_LIBROOT)/WebKit/JavaScriptCore
+LOC_LST_jsc_api := AllInOneFile
+include $(PRJ_LIBROOT)/build.mk
 
 #-------------------------------------------------------------------
 # Execute the build
