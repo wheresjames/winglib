223c7bc2be2dec26c9d078473924db11f0d8f5aa
rumbehant <rumbehant@38e6eff2-4521-0410-8f8e-9afd2f83d171>
2009-07-16 17:08:22 +0000

    ! Fixes for Windows + Added CUtil class
    git-svn-id: https://winglib.svn.sourceforge.net/svnroot/winglib/trunk@298 38e6eff2-4521-0410-8f8e-9afd2f83d171

---
+++ lib/oexlib/src/util.cpp
@@ -0,0 +1,51 @@
+/*------------------------------------------------------------------
+// util.cpp
+//
+// Copyright (c) 1997
+// Robert Umbehant
+// winglib@wheresjames.com
+// http://www.wheresjames.com
+//
+// Redistribution and use in source and binary forms, with or
+// without modification, are permitted for commercial and
+// non-commercial purposes, provided that the following
+// conditions are met:
+//
+// * Redistributions of source code must retain the above copyright
+//   notice, this list of conditions and the following disclaimer.
+// * The names of the developers or contributors may not be used to
+//   endorse or promote products derived from this software without
+//   specific prior written permission.
+//
+//   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND
+//   CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+//   INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+//   MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+//   DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+//   CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+//   SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+//   NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+//   LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+//   HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+//   CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+//   OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+//   EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//----------------------------------------------------------------*/
+
+#include "../oexlib.h"
+
+OEX_USING_NAMESPACE
+
+CStr8 CUtil::md5( CStr8 s )
+{	oexGUID hash;
+	return CBase16::Encode( oss::CMd5::Transform( &hash, s.Ptr(), s.Length() ), sizeof( hash ) );
+}
+
+CStrW CUtil::md5( CStrW s )
+{	oexGUID hash;
+	CStr8 sMb = oexStrWToMb( s );
+	CStrW _s = oexMbToStrW( CBase16::Encode( OEX_NAMESPACE::oss::CMd5::Transform( &hash, sMb.Ptr(), sMb.Length() ), sizeof( hash ) ) );
+  	return _s;
+}
+
+
