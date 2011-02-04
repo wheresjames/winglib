ba8bae48a663da4d35932eef6be2f3bec44d5a0b
ru <ru@wheresjames.com>
2011-02-04 13:23:44 -0500

    + linux

--- lib/oexlib/inc/oex_config.h
+++ lib/oexlib/inc/oex_config.h
@@ -293,7 +293,7 @@
 	typedef oexSIZE_T				oexNEWSIZE_T;
 #elif defined( OEX_CPU_64 ) || defined( OEX_IPHONE )
 	typedef long unsigned int		oexSIZE_T;
-	typedef size_t					oexNEWSIZE_T;
+	typedef oexSIZE_T				oexNEWSIZE_T;
 #else
 	typedef unsigned int			oexSIZE_T;
 	typedef oexSIZE_T				oexNEWSIZE_T;
