3378dbaeddf94699d05623a55cc9124b8635835c
ru <ru@wheresjames.com>
2011-07-22 17:39:57 -0400

    + Adding android example

--- app/hello_android/HelloWorld.java
+++ app/hello_android/HelloWorld.java
@@ -24,11 +24,6 @@ public class HelloWorld extends Activity
 	/// Native function
     public native String getString();
 
-    /* this is used to load the 'hello-jni' library on application
-     * startup. The library has already been unpacked into
-     * /data/data/com.example.HelloJni/lib/libhello-jni.so at
-     * installation time by the package manager.
-     */
     static 
 	{
         System.loadLibrary( "hello-world" );
