8d61704aff570bf4a764d17666551b5d2c9c2927
ru <ru@wheresjames.com>
2011-11-08 14:39:45 -0500

    + Qt WebKit build with msvc

--- tools/qtwk/main.cpp
+++ tools/qtwk/main.cpp
@@ -3,11 +3,13 @@
 #include "network.h"
 #include "mainwindow.h"
 
-
-// +++ Fix keyboard focus
-// +++ Chat window
-
-int main(int argc, char ** argv)
+#if defined(_WIN32_WCE)
+int WINAPI WinMain( HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLine, int nCmdShow )
+//#elif defined( OEX_GUI ) // Added qtmain.lib ref to windows.mk instead ;)
+//int APIENTRY WinMain( HINSTANCE hInstance, HINSTANCE hPrevInstance, char* pCmdLine, int nCmdShow )
+#else
+int main( int argc, char* argv[] )
+#endif
 {
 	// Init oexlib
 	oexINIT();
